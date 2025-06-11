7e8ea832d4dfa738679b3bb940cd0d7f0afb5dad
L050201 <1210843197@qq.com>
2025-06-11 15:24:54 +0800

    2
    Send packets and receive responses, and implement a timeout retransmission mechanism

--- udpclient.py
+++ udpclient.py
@@ -7,3 +7,7 @@ from typing import Optional, Tuple
 MAX_RETRIES =5
 INITIAL_TIMEOUT = 1.0
 #Initial Timeout (Seconds)
+
+def send_and_receive(socket: socket.socket, address: Tuple[str, int], data: bytes, operation: str) -> Optional[str]:
+    #Send packets and receive responses, and implement a timeout retransmission mechanism
+    
