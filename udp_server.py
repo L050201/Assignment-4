077c28dcb6e1be32d6115f09e8d44c3ed7fc02a2
L050201 <1210843197@qq.com>
2025-06-12 13:51:25 +0800

    2
    Handle the client's download request and create a new thread to manage data transfer.

--- udp_server.py
+++ udp_server.py
@@ -7,3 +7,6 @@ import random
 from typing import Tuple, Optional
 
 DATA_PORT_RANGE = (50000, 51000)  # range of ports to use for data transfer
+
+def handle_client_request(welcome_socket: socket.socket, request: str, client_address: Tuple[str, int]):
+    #Handle the client's download request and create a new thread to manage data transfer.
